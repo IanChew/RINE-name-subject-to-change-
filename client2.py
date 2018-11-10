0bc226adfbe5cd26f775ffc9d578ba6519417f26
Ian Chew <igac96@gmail.com>
2018-11-10 14:32:37 -0500

    Changed spaces to tabs.

--- client2.py
+++ client2.py
@@ -4,33 +4,31 @@ import socket
 import time, sys
 
 def Main():
-        host = input("what is the host ip?")
-        port = 5000
-        
-        mySocket = socket.socket()
-        mySocket.connect((host,port))
-        print("Connected to the mainframe. Please wait.")
-        
-        
-        message = ""
-        while True:
-            try:
-                while True:
-                    print("Waiting for message from server. Press Ctrl + c to send message")
-                    data = mySocket.recv(1024).decode()
-                    print ('Received from server: ' + data)
-            except KeyboardInterrupt:
-                pass
-            message = input("message (q to quit)")
-            if message == 'q':
-                break
-            send = message.upper()
-            print ("sending: " + str(send))
-            mySocket.send(send.encode())
+	host = input("what is the host ip?")
+	port = 5000
+	
+	mySocket = socket.socket()
+	mySocket.connect((host,port))
+	print("Connected to the mainframe. Please wait.")
+	
+	
+	message = ""
+	while True:
+		try:
+			while True:
+				print("Waiting for message from server. Press Ctrl + c to send message")
+				data = mySocket.recv(1024).decode()
+				print ('Received from server: ' + data)
+		except KeyboardInterrupt:
+			pass
+		message = input("message (q to quit)")
+		if message == 'q':
+			break
+		send = message.upper()
+		print ("sending: " + str(send))
+		mySocket.send(send.encode())
+		mySocket.close()
 
-        mySocket.close()
-
-
-        
+		
 if __name__ == '__main__':
-    Main()
+	Main()
--- server2.py
+++ server2.py
@@ -6,67 +6,69 @@ from threading import Thread
 import urllib.request
 
 def Main():
-    port = 5000
-    host = socket.gethostname()
-    
-    mySocket = socket.socket()
-    
-    mySocket.bind((host,port))
-    print('your ip is :' , urllib.request.urlopen('https://ident.me/').read().decode('utf8'))
-    connections = int(input("How many people are watching with you?"))
-    mySocket.listen(connections)
-    
-    peers = []
-    try:
-        while connections > 0:
-            conn, addr = mySocket.accept()
-            peers.append(user(conn, addr))
-            print ("Connection from: " , str(addr))
-            connections -= 1
-    except KeyboardInterrupt:
-        pass
-    
-    
-    print("All clients have connected. You may now proceed")
-    
-    for peer in peers:
-        t = Thread(target=listen, args = (peer,peers,))
-        t.start()
-    
-    while True:
-        data = input(" -> ")
-        for client in peers:
-            client.get_connection().send(data.encode())
-        if data.upper() == 'PLAY' or data.upper() == 'STOP':
-            time.sleep(0.1)
-                
+	port = 5000
+	host = socket.gethostname()
+	
 
-    mySocket.close()
+
+	mySocket = socket.socket()
+	
+	mySocket.bind((host,port))
+	print('your ip is :' , urllib.request.urlopen('https://ident.me/').read().decode('utf8'))
+	connections = int(input("How many people are watching with you?"))
+	mySocket.listen(connections)
+	
+	peers = []
+	try:
+		while connections > 0:
+			conn, addr = mySocket.accept()
+			peers.append(user(conn, addr))
+			print ("Connection from: " , str(addr))
+			connections -= 1
+	except KeyboardInterrupt:
+		pass
+	
+	
+	print("All clients have connected. You may now proceed")
+	
+	for peer in peers:
+		t = Thread(target=listen, args = (peer,peers,))
+		t.start()
+	
+	while True:
+		data = input(" -> ")
+		for client in peers:
+			client.get_connection().send(data.encode())
+		if data.upper() == 'PLAY' or data.upper() == 'STOP':
+			time.sleep(0.1)
+				
+
+	mySocket.close()
 def listen(peer, peers):
-    while True:
-        
-        print("Waiting for message from clients.")
-        data = peer.get_connection().recv(1024).decode()
-        print ('Received from client: ' + data)
-        for client in peers:
-            client.get_connection().send(data.encode())
-        if data.upper() == 'PLAY' or data.upper() == 'STOP':
-            time.sleep(0.1)
-                    
-          
-            
+	while True:
+		
+		print("Waiting for message from clients.")
+		data = peer.get_connection().recv(1024).decode()
+		print ('Received from client: ' + data)
+		for client in peers:
+			client.get_connection().send(data.encode())
+		if data.upper() == 'PLAY' or data.upper() == 'STOP':
+			time.sleep(0.1)
+					
+		  
+			
 class user:
-    
-    def __init__(self, conn, addr):
-        self.connection = conn
-        self.address = addr
-    
-    def get_connection(self):
-        return self.connection
-        
-    def get_address(self):
-        return self.address
-        
-        
+	
+	def __init__(self, conn, addr):
+		self.connection = conn
+		self.address = addr
+	
+	def get_connection(self):
+		return self.connection
+		
+	def get_address(self):
+		return self.address
+		
+		
 if __name__ == '__main__':
-    Main()
+	Main()
