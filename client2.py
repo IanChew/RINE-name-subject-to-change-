65c6a4447c84362ebbbd4faaedb94396d01fa1fb
Ian Chew <igac96@gmail.com>
2018-11-10 14:50:36 -0500

    Fixed the control flow in the client loop, made it based on a with statement, and got basic server-client echoing working.

--- client2.py
+++ client2.py
@@ -4,30 +4,32 @@ import socket
 import time, sys
 
 def Main():
-	host = input("what is the host ip?")
+	host = input("what is the host ip?\n")
 	port = 5000
 	
-	mySocket = socket.socket()
-	mySocket.connect((host,port))
-	print("Connected to the mainframe. Please wait.")
+	with socket.socket() as mySocket:
+		mySocket.connect((host,port))
+		print("Connected to the mainframe. Please wait.")
 	
-	
-	message = ""
-	while True:
-		try:
-			while True:
-				print("Waiting for message from server. Press Ctrl + c to send message")
-				data = mySocket.recv(1024).decode()
-				print ('Received from server: ' + data)
-		except KeyboardInterrupt:
-			pass
-		message = input("message (q to quit)")
-		if message == 'q':
-			break
-		send = message.upper()
-		print ("sending: " + str(send))
-		mySocket.send(send.encode())
-		mySocket.close()
+#		message = ""
+		while True:
+#			try:
+#				while True:
+#					print("Waiting for message from server. Press Ctrl + c to send message")
+#					data = mySocket.recv(1024).decode()
+#					print ('Received from server: ' + data)
+#			except KeyboardInterrupt:
+#				pass
+			message = input("message (q to quit)\n")
+			if message == 'q':
+				break
+			send = message.upper()
+			print("sending:", str(send))
+			mySocket.send(send.encode())
+
+			print("Waiting for message from server. Press Ctrl + c to send message")
+			data = mySocket.recv(1024).decode()
+			print('Received from server:', data)
 
 		
 if __name__ == '__main__':
