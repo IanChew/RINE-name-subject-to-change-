4f5d84d986689820f2587ff42d69588cb5e15cd7
Ian Chew <igac96@gmail.com>
2018-11-10 19:52:59 -0500

    Put the client's input in a seperate thread.

--- client2.py
+++ client2.py
@@ -2,6 +2,15 @@
 
 import socket
 import time, sys
+import threading
+
+# Blocks on input from the server.
+def input_thread(mySocket):
+	while True:
+#		print("Waiting for message from server...")
+		data = mySocket.recv(1024).decode()
+		print('Received from server:', data)
+
 
 def Main():
 	host = input("what is the host ip?\n")
@@ -9,7 +18,14 @@ def Main():
 	
 	with socket.socket() as mySocket:
 		mySocket.connect((host,port))
-		print("Connected to the mainframe. Please wait.")
+		print("Connected to the server at", host)
+
+		# Start input_thread in a new thread.
+		ithread = threading.Thread(target=input_thread, args=(mySocket,))
+		# Our thread dies when the main thread exits.
+		ithread.daemon = True
+		# Start the input thread.
+		ithread.start()
 	
 #		message = ""
 		while True:
@@ -20,16 +36,13 @@ def Main():
 #					print ('Received from server: ' + data)
 #			except KeyboardInterrupt:
 #				pass
-			message = input("message (q to quit)\n")
+			message = input() #"message (q to quit)\n")
 			if message == 'q':
 				break
 			send = message.upper()
 			print("sending:", str(send))
 			mySocket.send(send.encode())
 
-			print("Waiting for message from server...")
-			data = mySocket.recv(1024).decode()
-			print('Received from server:', data)
 
 		
 if __name__ == '__main__':
